6df91f672ba8eb0910609e6e4885e3292bca0967
angel-15 <angelrestifo36@gmail.com>
2021-07-01 21:33:38 +0000

    Todo list

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,62 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+export class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tasks: [
+				"Hacer la cama",
+				"Hacer el desayuno",
+				"Alimentar a Lola",
+				"Limpiar la caja de arena"
+			],
+			newTask: ""
+		};
+
+		this.handleChangeTask = this.handleChangeTask.bind(this);
+		this.handleAddTask = this.handleAddTask.bind(this);
+	}
+
+	handleChangeTask(event) {
+		/* eslint-disable no-console */
+		//console.log(event);
+		this.setState({
+			...this.state.tasks,
+			newTask: event.target.value
+		});
+	}
+
+	handleAddTask(event) {
+		if (event.key == "Enter" && this.state.newTask.length > 4) {
+			console.log(...this.state.tasks);
+			this.setState({
+				tasks: [...this.state.tasks, this.state.newTask],
+				newTask: ""
+			});
+		}
+	}
+
+	render() {
+		console.log(this.state);
+		return (
+			<div className="text-center mt-5">
+				<h1>las tareas de 4geeks</h1>
+
+				<input
+					Value={this.state.newTask}
+					placeholder="Escribe tu tarea acá"
+					onChange={event => this.handleChangeTask(event)}
+					onKeyPress={event => this.handleAddTask(event)}
+				/>
+				<ul>
+					{this.state.tasks.map((task, index) => {
+						console.log(task, index);
+						return <li key={index}>{task}</li>;
+					})}
+				</ul>
+				<footer>Tareas por hacer: {this.state.tasks.length}</footer>
+			</div>
+		);
+	}
 }
