b32054aa85788224c61e978ad782c6cda9447548
angel-15 <angelrestifo36@gmail.com>
2021-07-03 00:27:49 +0000

    todo list

--- src/js/component/TasksHook.js
+++ src/js/component/TasksHook.js
@@ -1,82 +1,84 @@
 import React, { useState, useEffect } from "react";
+import { Button } from "@material-ui/core";
 
 const TasksHook = () => {
-    //useState para manipular el estado, cada estado es manipulado de manera individual
-    const [Tasks, settasks] = useState([]);
-    const [newTask, setNewTask] = useState(" ");
-    const [update, setUpdate] = useState(false);
+	//useState para manipular el estado, cada estado es manipulado de manera individual
+	const [tasks, setTasks] = useState([]);
+	const [newTask, setNewTask] = useState("");
+	const [update, setUpdate] = useState(false);
 
-    console.log(tasks);
+	console.log(tasks);
 
-    const handleAddTask = event => {
-        if (event.key == "Enter" && newTask.length > 4) {
-            console.log(...tasks);
-            settasks([...tasks, newTask]);
-            setNewTask("");
-        }
-    };
+	const handleAddTask = event => {
+		if (event.key == "Enter" && newTask.length > 4) {
+			console.log(...tasks);
+			setTasks([...tasks, newTask]);
+			setNewTask("");
+		}
+	};
 
-    const handleDeleteTask = indexTask => {
-        let filteredTasks = tasks.filter(
-            (currentTask, indexCurrentTask) => indexCurrentTask !== indexTask
-        );
-        console.log(filteredTasks, indexTask, tasks);
-        settasks(filteredTasks);
-    };
+	const handleDeleteTask = indexTask => {
+		let filteredTasks = tasks.filter(
+			(currentTask, indexCurrentTask) => indexCurrentTask !== indexTask
+		);
+		console.log(filteredTasks, indexTask, tasks);
+		setTasks(filteredTasks);
+	};
 
-    useEffect(() => {
-        console.log("componentDidMount");
-        // Todo lo que está dentro del setTimeout es la fase de montaje
-        setTimeout(
-            () =>
-                setTasks([
-                    "Hacer la cama",
-                    "Hacer el desayuno",
-                    "Alimentar a Lola",
-                    "Limpiar la caja de arena"
-                ]),
-            3000
-        );
-        const interval = setInterval(() => {
-            console.log("This will run every second!", update);
-            setUpdate(prev => !prev);   
-        }, 3000);
-        return () => clearInterval(interval); // fase de desmontaje
-    }, [update]); //Fase de actualización
+	useEffect(() => {
+		console.log("componentDidMount");
+		// Todo lo que está dentro del setTimeout es la fase de montaje
+		setTimeout(
+			() =>
+				setTasks([
+					"Hacer la cama",
+					"Hacer el desayuno",
+					"Alimentar a Lola",
+					"Limpiar la caja de arena"
+				]),
+			3000
+		);
+		const interval = setInterval(() => {
+			console.log("This will run every second!", update);
+			setUpdate(prev => !prev);
+		}, 3000);
+		return () => clearInterval(interval); // fase de desmontaje
+	}, [update]); //Fase de actualización
 
-    return ( 
-        <div className="text-center mt-5">
-            <h1>las tareas de 4geeks (Hooks)</h1>
+	return (
+		<div className="text-center mt-5">
+			<h1>las tareas de 4geeks (Hooks)</h1>
 
-            <input
-                Value={newTask}
-                placeholder="Escribe tu tarea acá"
-                onChange={event => setNewTask(event.target.value)}
-                onKeyPress={event => handleAddTask(event)}
-            />
-            <ul>
-                {tasks.length ? tasks.map((task, index) => {
-                        console.log(task, index);
-                        return (
-                            <li key={index}>
-                                {task}
-                                <button
-                                    type="button"
-                                    onClick={() => handleDeleteTask(index)}>
-                                    X
-                                </button>
-                            </li>
-                        );
-                    })
-                : null}
-            </ul>
-            {tasks.length > 0 ? (
-                <footer>Tareas por hacer: {this.state.tasks.length}</footer>
-            ) : (<footer>Estás libre de tareas!!!!!!!</footer>)}
-        </div>
-        
-    );
-
+			<input
+				Value={newTask}
+				placeholder="Escribe tu tarea acá"
+				onChange={event => setNewTask(event.target.value)}
+				onKeyPress={event => handleAddTask(event)}
+			/>
+			<ul>
+				{tasks.length
+					? tasks.map((task, index) => {
+							console.log(task, index);
+							return (
+								<li key={index}>
+									{task}
+									<button
+										type="button"
+										onClick={() => handleDeleteTask(index)}>
+										X
+									</button>
+								</li>
+							);
+					  })
+					: null}
+			</ul>
+			{tasks.length > 0 ? (
+				<footer>Tareas por hacer: {tasks.length}</footer>
+			) : (
+				<footer>Estás libre de tareas!!!!!!!</footer>
+			)}
+		</div>
+	);
 };
 
 export default TasksHook;
